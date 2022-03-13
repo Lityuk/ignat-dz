e14c81e3f086d80a8b44372514a8fa9687115a37
Lityuk <lityuk@gmail.com>
2022-03-14 01:43:45 +0300

    feat: add basic compomemts: junior and juniorPlus

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+import MyRoutes from './MyRoutes'
+import { HashRouter } from 'react-router-dom'
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
-            <Header/>
 
-            <Routes/>
+                <Header/>
+                <MyRoutes/>
 
-            {/*</HashRouter>*/}
+
+            </HashRouter>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export function JuniorPlus() {
+    return (
+        <div>
+            Hello JuniorPlus!
+
+        </div>
+    )
+}
+
+
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
